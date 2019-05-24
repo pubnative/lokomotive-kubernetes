74f62838dc5b0e952753f7822f4c442bc3fcd165
Kai Lüke <kai@kinvolk.io>
2019-05-24 13:08:07 +0200

    Introduce kvm-libvirt platform

---
+++ kvm-libvirt/flatcar-linux/kubernetes/require.tf
@@ -0,0 +1,31 @@
+# Terraform version and plugin versions
+
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "ct" {
+  version = "~> 0.3"
+}
+
+provider "local" {
+  version = "~> 1.0"
+}
+
+provider "null" {
+  version = "~> 1.0"
+}
+
+provider "template" {
+  version = "~> 1.0"
+}
+
+provider "tls" {
+  version = "~> 1.0"
+}
+
+provider "libvirt" {
+  # TODO: add version once released
+  uri = "qemu:///system"
+}
+
